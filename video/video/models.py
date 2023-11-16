8103bb35d77d5185febc1b3d4e756ebb9b9b82a6
nq2623up <DakotaBruffett@gmail.com>
2023-11-16 13:27:12 -0600

    server and model/ admin work

--- video/video/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class video(models.model):
-    name = models.CharField(max_length= 200)
-    url = models.CharField(max_length=400)
-    notes = models.TextField(blank=True, null= False)
-def __str__(self):
-    return f'id:{self.pk}, Name: {self.name} , url: {self.url} ,NOTES: {self.notes}(:200)'
--- video/video_collection/models.py
+++ video/video_collection/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class video(models.model):
+class Video(models.Model):
     name = models.CharField(max_length= 200)
     url = models.CharField(max_length=400)
     notes = models.TextField(blank=True, null= False)
